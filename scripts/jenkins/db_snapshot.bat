66d6d809ce222631443ee22a42986fafdaf8bbd4
naveenthe9 <naveenkumarb@ibaseit.com>
2025-06-12 17:20:30 +0530

    second commit

--- scripts/jenkins/db_snapshot.bat
+++ scripts/jenkins/db_snapshot.bat
@@ -1,9 +1,10 @@
 @echo off
 set TF_VAR_snapshot_date=%5
 echo %TF_VAR_snapshot_date%
+set AWS_PROFILE=default
 echo %5 > file.txt
 terraform init
 terraform plan
 
 
-rem terraform apply --auto-approve
+terraform apply --auto-approve
