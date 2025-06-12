63aaf6b42e0942daba60c52c7cfdc9acaf89b2ae
naveenthe9 <naveenkumarb@ibaseit.com>
2025-06-12 17:56:13 +0530

    second commit

--- scripts/jenkins/db_snapshot.bat
+++ scripts/jenkins/db_snapshot.bat
@@ -1,4 +1,5 @@
 @echo off
+cd ../terraform/
 set TF_VAR_snapshot_date=%5
 echo %TF_VAR_snapshot_date%
 set AWS_PROFILE=default
--- scripts/jenkins/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "snapshot_date" {
-  description = "Date used for naming the DB snapshot"
-  type        = string
-}
