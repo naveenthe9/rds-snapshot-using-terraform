5330d7f6ea15fdbce02cf8626a088744d0fdb417
naveenthe9 <naveenkumarb@ibaseit.com>
2025-06-12 16:33:17 +0530

    second commit

--- scripts/jenkins/main.tf
+++ scripts/jenkins/main.tf
@@ -7,6 +7,9 @@ terraform {
   }
 }
 
+provider "aws" {
+
+}
 resource "aws_db_cluster_snapshot" "development-database-snapshot" {
 db_cluster_identifier = "sttaq-preprod"
 db_cluster_snapshot_identifier = "development-database-snapshot-${var.snapshot_date}"
